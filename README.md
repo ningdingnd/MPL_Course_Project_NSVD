38f1f4f74a0beacf6d0ccc522199e0a3102c1531
qiansuyu <117184108+qiansuyu319@users.noreply.github.com>
2023-12-04 20:33:03 +0100

    This is the 72 version

---
+++ README.md
@@ -0,0 +1,18 @@
+# Instructions
+
+## Data
+1. The data needed for this re-implementation task is located in the ```data/``` folder.
+2. There you can find the raw dialogs with the ground truth caption and question programs (```data/dialogs.json```) and the derendered scenes on top of which the programs will be executed (```data/CLEVR_scenes.json```).
+
+## Code
+Please run the code using ```main.py``` script. It will automatically set up the paths to the data and instantiate an object of the executor.
+
+## Task 
+Some functions of the executor are missing. The main goal of this task is to re-implement these based on the NSVD [paper][1] and the remaining methods of the executor. Please take a look at ```executor/symbolic_executor.py``` and complete the missing modules. These are marked with "TODO".
+
+## Goal
+Once you are happy with the code, run the evaluation script, i.e. ```main.py```, to test you implemented logic. You should get an accuracy close to that in Table 6 of the [paper][1], i.e. 99.99%
+
+HAPPY CODING!
+
+[1]: https://perceptualui.org/publications/abdessaied22_coling.pdf
---
+++ main.py
@@ -0,0 +1,57 @@
+import os
+import json
+from utils import load_clevr_scenes
+from executor.symbolic_executor import SymbolicExecutorClevr
+
+from tqdm import tqdm
+
+path_scenes_raw = "/Users/qiansuyu/Desktop/NSVD_reimplementation/NSVD/data/CLEVR_scenes.json"
+path_dataset = "/Users/qiansuyu/Desktop/NSVD_reimplementation/NSVD/data/dialogs.json"
+with open(path_dataset, "r") as file:
+    dataset = json.load(file)
+
+symblicExecutor = SymbolicExecutorClevr(path_scenes_raw)
+scenes = load_clevr_scenes(path_scenes_raw)
+
+def test():
+    numFalse = 0
+    numAll = 0
+    pbar = tqdm(dataset)
+    pbar.set_description("[Evaluation in Progress]")
+    for img in pbar:
+        imgIdx = img["image_index"]
+        dialogs = img["dialogs"]
+        for dialog in dialogs:
+            symblicExecutor.reset(imgIdx)
+            # execute the prog. from the caption
+            captionFuncLabel = dialog["template"]
+            print("The caption function is ",captionFuncLabel)
+            captionFuncArgs = list(
+                map(lambda a: "_".join(a.split(" ")), dialog["args"]))
+            
+            symblicExecutor.execute(captionFuncLabel, captionFuncArgs)
+
+            # Answer the questions
+            for i, d in enumerate(dialog["dialog"]):
+                print("The question is ",i+1,"Question and the content is ",d["question"])
+                print("The type is ",d["template"])
+                # if i+1 == 10:
+                #     print("bla")
+                numAll += 1
+                questionFuncLabel = d["template"]
+                questionFuncArgs = list(
+                    map(lambda a: "_".join(a.split(" ")), d["args"]))
+                gtAnswer = "_".join(str(d["answer"]).split(" "))
+                predAnswer = symblicExecutor.execute(
+                    questionFuncLabel, questionFuncArgs)
+                predAnswer = str(predAnswer)
+                if predAnswer != gtAnswer:
+         
+                    print("Predict answer",predAnswer)
+                    print("GT answer is ",gtAnswer)
+                    numFalse += 1
+             
+    print("acc = {}".format(1 - numFalse/numAll))
+
+if __name__ == "__main__":
+    test()
